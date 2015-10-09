796dceae8d241c6b1fef2f9afb7b4a44eb333018
Morgan Lieberthal <morgan@jmorgan.org>
2015-10-09 13:12:05 -0600

    custom generator cookbook

---
+++ cookbooks/code_generator/recipes/repo.rb
@@ -0,0 +1,48 @@
+context = ChefDK::Generator.context
+repo_dir = File.join(context.repo_root, context.repo_name)
+
+# repo root dir
+directory repo_dir
+
+# Top level files
+template "#{repo_dir}/LICENSE" do
+  source "LICENSE.#{context.license}.erb"
+  helpers(ChefDK::Generator::TemplateHelper)
+end
+
+cookbook_file "#{repo_dir}/README.md" do
+  source "repo/README.md"
+end
+
+cookbook_file "#{repo_dir}/chefignore" do
+  source "chefignore"
+end
+
+%w{cookbooks data_bags environments roles}.each do |tlo|
+  remote_directory "#{repo_dir}/#{tlo}" do
+    source "repo/#{tlo}"
+  end
+end
+
+cookbook_file "#{repo_dir}/cookbooks/README.md" do
+  if context.policy_only
+    source "cookbook_readmes/README-policy.md"
+  else
+    source "cookbook_readmes/README.md"
+  end
+end
+
+# git
+if context.have_git
+  if !context.skip_git_init
+    execute("initialize-git") do
+      command("git init .")
+      cwd repo_dir
+      not_if { File.exist?("#{repo_dir}/.gitignore") }
+    end
+  end
+  template "#{repo_dir}/.gitignore" do
+    source "repo/gitignore.erb"
+    helpers(ChefDK::Generator::TemplateHelper)
+  end
+end
