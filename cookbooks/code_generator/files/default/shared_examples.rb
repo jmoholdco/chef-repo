92324e09f6e7c3fbd8180d53711856170a21cac8
Morgan Lieberthal <morgan@jmorgan.org>
2015-10-23 22:55:07 -0600

    Moving code_generator to its own repo

--- cookbooks/code_generator/files/default/shared_examples.rb
+++
@@ -1,5 +0,0 @@
-RSpec.shared_examples 'converges successfully' do
-  it 'converges successfully' do
-    expect { chef_run }.to_not raise_error
-  end
-end
--- cookbooks/code_generator/metadata.rb
+++
@@ -1,4 +0,0 @@
-name             'code_generator'
-description      'Custom code generator cookbook for use with ChefDK'
-long_description 'Custom code generator cookbook for use with ChefDK'
-version          '0.1.0'
